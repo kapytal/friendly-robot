9c5712fbe9658a2d360d9c12aa71156692a82fc2
OZARGA <OZARGA@LAPTOP-OZARGA>
2019-08-17 14:39:42 -0500

    Testing commit

---
+++ Test Suites/TestSuite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2019-08-16T17:36:09</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0dff55a1-ded3-4a3f-b523-9ae1f5c55ec4</testSuiteGuid>
+   <testCaseLink>
+      <guid>75d5d5b4-985d-4d0a-b543-805f132c303c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CaseNoSpace</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
